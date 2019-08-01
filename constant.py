a83d6b0b1543a69f37d2d9c8d658d6d36c72cd0b
carriex <carriex@pn-204-115.itsc.cuhk.edu.hk>
2019-08-01 08:29:00 +0800

    Refactor the code

---
+++ constant.py
@@ -0,0 +1,27 @@
+BASE_LR = 0.001
+MOMENTUM = 0.9 
+NUM_EPOCHES = 18
+WEIGHT_DECAY = 0.005
+TRAIN_BATCH_SIZE = 1
+TEST_BATCH_SIZE = 12
+LR_DECAY_STEP_SIZE = 6
+LR_DECAY_GAMMA = 0.1
+
+NUM_CLASSES = 101 
+NUM_MOTION_LABEL = 14
+
+CLIP_LENGTH = 16
+CROP_SIZE = 112
+RESIZE_H = 171
+RESIZE_W = 128
+
+TEST_LIST = 'list/test_ucf101.list'
+TRAIN_LIST = 'list/rgb_list.list'
+FRAMELIST_FILE = 'list/rgb_list.list'
+V_FLOW_LIST_FILE = 'list/v_flow_list.list'
+U_FLOW_LIST_FILE = 'list/u_flow_list.list'
+MODEL_DIR = 'models'
+TRAINED_MODEL = 'c3d-finetune.pth-60000'
+TRAIN_MODEL_NAME = 'c3d-finetune.pth'
+PRETRAINED_MODEL = 'c3d-motion.pth-60000'
+PRETRAIN_MODEL_NAME = 'c3d-motion.pth'
--- train.py
+++ train.py
@@ -1,45 +1,36 @@
+import os
 import torch.optim as optim 
 import torch.nn as nn 
-import torch, torchvision
-import model
-import os
-import time
-from dataset import UCF101DataSet
+import torch
 from tensorboardX import SummaryWriter
+import model
+import constant
+from dataset import UCF101DataSet
+from utils import get_default_device
 
-
-base_lr = 0.001
-momentum = 0.9 
-batch_size = 30
-num_classes = 14
-num_epoches = 18
-weight_decay = 0.005
-framelist_file = 'list/rgb_train_linux.list'
-v_flow_list_file='list/v_flow_train_linux.list'
-u_flow_list_file='list/u_flow_train_linux.list'
-clip_len = 16
-model_dir = 'models'
-model_name = 'c3d-motion-new.pth'
-os.environ['CUDA_VISIBLE_DEVICES'] = '2'
 
 
 def train():
 
 	#initialize the model
-	c3d = model.C3D(num_classes,pretrain=True)
+	c3d = model.C3D(constant.NUM_MOTION_LABEL,pretrain=True)
 
-	train_param = [
-					{'params':c3d.get_1x_lr_param()},
-					{'params':c3d.get_2x_lr_param(), 'lr': base_lr*2}]
+	train_param = [{'params':c3d.get_1x_lr_param()}, 
+				   {'params':c3d.get_2x_lr_param(), 'lr': constant.BASE_LR*2}]
 
 	device = get_default_device()
-	print(device)
 
-	print(c3d)
 
 	#import input data
-	trainset = UCF101DataSet(framelist_file=framelist_file, v_flow_list_file=v_flow_list_file, u_flow_list_file=u_flow_list_file,clip_len=clip_len, crop_size=112,split="training")
-	trainloader = torch.utils.data.DataLoader(trainset,batch_size=batch_size,shuffle=True,num_workers=10)
+	trainset = UCF101DataSet(framelist_file=constant.FRAMELIST_FILE, 
+		                     v_flow_list_file=constant.V_FLOW_LIST_FILE, 
+		                     u_flow_list_file=constant.U_FLOW_LIST_FILE,
+		                     clip_len=constant.CLIP_LENGTH, 
+		                     crop_size=constant.CROP_SIZE,
+		                     split="training")
+
+	trainloader = torch.utils.data.DataLoader(trainset, batch_size=constant.TRAIN_BATCH_SIZE, 
+		                                      shuffle=True, num_workers=10)
 	
 
 	c3d.to(device, non_blocking=True,dtype=torch.float)
@@ -51,23 +42,26 @@ def train():
 
 
 	#define optimizer 
-	optimizer = optim.SGD(train_param, lr=base_lr, momentum=momentum, weight_decay=weight_decay)
+	optimizer = optim.SGD(train_param, lr=constant.BASE_LR, 
+		                               momentum=constant.MOMENTUM, 
+		                               weight_decay=constant.WEIGHT_DECAY)
 
 	#lr is divided by 10 after every 4 epoches 
 	
-	scheduler = optim.lr_scheduler.StepLR(optimizer,step_size=6, gamma=0.1)
+	scheduler = optim.lr_scheduler.StepLR(optimizer,step_size=constant.LR_DECAY_STEP_SIZE, 
+		                                            gamma=constant.LR_DECAY_GAMMA)
 
 	writer = SummaryWriter()
 
-	for epoch in range(num_epoches):
+	for epoch in range(constant.NUM_EPOCHES):
 		
 		running_loss = 0.0 
-		scheduler.step() #last_epoch default is -1
+		scheduler.step() 
 
 		for i, data in enumerate(trainloader, 0):
 
 			step = epoch * len(trainloader) + i
-			inputs, labels = data['clip'].to(device,dtype=torch.float), data['label'].to(device)
+			inputs, labels = data['clip'].to(device, dtype=torch.float), data['label'].to(device)
 			optimizer.zero_grad()
 
 			
@@ -87,16 +81,12 @@ def train():
 					(epoch + 1, i + 1, running_loss / 100))
 				running_loss = 0.0 
 			if step % 10000 == 9999:
-				torch.save(c3d.state_dict(),os.path.join(model_dir,'%s-%d'%(model_name, step+1)))
+				torch.save(c3d.state_dict(),
+					       os.path.join(constant.MODEL_DIR,'%s-%d'%(constant.TRAIN_MODEL_NAME, step+1)))
 
 	print('Finished Training')
 	writer.close()
 
-def get_default_device():
-	if torch.cuda.is_available():
-		return torch.device('cuda:0')
-	else:
-		return torch.device('cpu')
 
 
 
